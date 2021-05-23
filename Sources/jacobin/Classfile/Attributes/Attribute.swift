6e547a4ee02c0475de861c3a3d348b55348766bb
platypusguy <platypus@pz.org>
2021-05-22 22:47:42 -0700

    Minor cleanup of comments

--- Sources/jacobin/Classfile/Attributes/Attribute.swift
+++ Sources/jacobin/Classfile/Attributes/Attribute.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 
-// Classes for all the attributes found in class files
+// Superclass for all the attributes found in class files
 class Attribute {
     var attrName = ""
     var attrLength = 0
@@ -22,32 +22,3 @@ class Attribute {
 class AttributeInfo: Attribute {
     var attrInfo : [UInt8] = []
 }
-
-//class CodeAttribute: Attribute {
-//    /**
-//      u2 max_stack;
-//    u2 max_locals;
-//    u4 code_length;
-//    u1 code[code_length];
-//    u2 exception_table_length;
-//    {   u2 start_pc;
-//        u2 end_pc;
-//        u2 handler_pc;
-//        u2 catch_type;
-//    } exception_table[exception_table_length];
-//    u2 attributes_count;
-//    attribute_info attributes[attributes_count];
-//     */
-//    var maxStack = 0
-//    var maxLocals = 0
-//    var codeLength = 0
-//    var exceptionTableLength = 0
-//    struct ExceptionEntry {
-//        var startPc = 0
-//        var handlerPc = 0
-//        var catchType = 0
-//    }
-//    var exceptionTable : [ExceptionEntry] = []
-//    var codeAttrCount = 0
-//    var codeAttrTable : [AttributeInfo] = []
-//}
